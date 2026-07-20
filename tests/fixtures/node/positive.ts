ca7227173f5b104cde111b31e791a83e639593fa
DevilTea <ch19980814@gmail.com>
2026-07-20 17:24:44 +0800

    test: remove the transitional Node source

--- tests/fixtures/node/positive.ts
+++
@@ -1,5 +0,0 @@
-import { readFile } from 'node:fs/promises'
-import process from 'node:process'
-
-export const platform = process.platform
-export const readText = (path: string): Promise<string> => readFile(path, 'utf8')
